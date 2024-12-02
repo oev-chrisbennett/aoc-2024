31e326a4721d301703aefe61c8c8352b0f7752cb
Chris Bennett <110393183+oev-chrisbennett@users.noreply.github.com>
2024-12-02 08:21:39 +0000

    feat(day 02): part 1+2

--- src/solutions/02.ts
+++ src/solutions/02.ts
@@ -1,10 +1,36 @@
-const file = Bun.file("src/inputs/day02.txt")
-const input = await file.text()
+const isSafe = ({ levels }: { levels: number[] }): boolean => {
+    const differences: number[] = []
 
-for (const line of input.split("\n")) {
-    if (!line) {
-        continue
+    for (let i = 1; i < levels.length; i++) {
+        differences.push(levels[i] - levels[i - 1])
     }
+
+    const increasing = differences.every((d) => d >= 1 && d <= 3)
+    const decreasing = differences.every((d) => d <= -1 && d >= -3)
+
+    return increasing || decreasing
 }
 
-console.log(`Day 02, Part 1: ${"solution"}, Part 2: ${"solution"}`)
+const input = await Bun.file("src/inputs/day02.txt").text()
+const reports = input.split("\n").map((line) => line.split(" ").map(Number))
+
+let safe = 0
+let madeSafe = 0
+
+for (const report of reports) {
+    let tolerable = false
+
+    for (let i = 0; i < report.length; i++) {
+        const removed = [...report.slice(0, i), ...report.slice(i + 1)]
+
+        if (isSafe({ levels: removed })) {
+            tolerable = true
+            break
+        }
+    }
+
+    if (isSafe({ levels: report })) safe++
+    if (isSafe({ levels: report }) || tolerable) madeSafe++
+}
+
+console.log({ part1: safe, part2: madeSafe })
